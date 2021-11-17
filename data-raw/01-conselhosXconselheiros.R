21a0a6e85979e50347a728dc0e906714c2bf26c7
David Maciel de Mello Neto <davidmaciel2206@gmail.com>
2021-11-17 14:18:05 -0300

    modificacao fontes

--- data-raw/01-conselhosXconselheiros.R
+++ data-raw/01-conselhosXconselheiros.R
@@ -1,3 +1,9 @@
 ## code to prepare `conselhosXconselheiros` dataset goes here
+if(!"tidyverse" %in% rownames(installed.packages())){
+  install.packages("tidyverse")
+}
+library(tidyverse)
+
+cons <- read.csv2("data-raw/sources/conselheiros.CSV")
 
 usethis::use_data(conselhosXconselheiros, overwrite = TRUE)
